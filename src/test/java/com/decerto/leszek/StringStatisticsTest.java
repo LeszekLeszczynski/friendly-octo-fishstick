458271bca92faa1b52c428109fc569e06229d5ff
Leszek Leszczynski <leszek.leszczynski@decerto.pl>
2023-11-13 12:15:11 +0100

    Updated StringStatisticsTest dsc

--- src/test/java/com/decerto/leszek/StringStatisticsTest.java
+++ src/test/java/com/decerto/leszek/StringStatisticsTest.java
@@ -25,7 +25,13 @@ public class StringStatisticsTest {
     }
 
     /**
-     * TODO: this test passes, but the method under test fails in some cases. Expand the test to include more cases and fix the method under test.
+     * TODO: this test passes, but the method under test is not implemented properly.
+     * As per requirements:
+     * The method should return a map of letters and their counts.
+     * The method should ignore non-letter characters.
+     * The method should ignore case.
+     * The method should return an empty map if the list of strings is empty.
+     * For some reason, if "Dave is stupid" is passed in the list, the method should ignore this string
      */
     @Test
     void testGetLetterStatistics() {
